841561068b0ce5909af3d3dd3674bfbf36d57892
danielortegac <deoc29@gmail.com>
2025-08-10 22:22:45 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Goatify IA SW – v29  */
+/* Goatify IA SW – v30  */
 const CACHE_VERSION = 26;
 const CACHE_NAME = `goatify-cache-v${CACHE_VERSION}`;
 const CORE_ASSETS = ['/', '/index.html', '/manifest.json', '/logos-hd.png'];
